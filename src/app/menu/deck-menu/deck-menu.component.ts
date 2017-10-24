23b45624777f13e9163fac224d4612b58f8e520b
LeoKramer <kramer.leo.94@gmail.com>
2017-10-24 19:45:15 -0300

    Arrumado criação do documento da conta do usuário com o primeiro baralho gerado automaticamente

--- src/app/menu/deck-menu/deck-menu.component.ts
+++ src/app/menu/deck-menu/deck-menu.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
 import {AuthService} from '../../auth/auth.service';
 import { moveIn, fallIn } from '../../router.animations';
 
@@ -7,11 +7,20 @@ import { moveIn, fallIn } from '../../router.animations';
   templateUrl: './deck-menu.component.html',
   styleUrls: ['./deck-menu.component.css']
 })
-export class DeckMenuComponent implements OnInit {
+export class DeckMenuComponent implements OnInit, AfterViewInit {
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService) {
+  }
 
   ngOnInit() {
+  		//image.src = "assets/images/placeholder_createdDeck.png";
+  }
+
+  ngAfterViewInit(){
+  	//console.log("after view init");
+
+  	//var image = document.getElementById("imgDeck1");
+  	//console.log(image);
   }
 
   logout() {
--- src/app/menu/main-menu/main-menu.component.ts
+++ src/app/menu/main-menu/main-menu.component.ts
@@ -7,11 +7,33 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 interface Users{
-  deck1: string;
-  deck2: string;
-  deck3: string;
-  deck4: string;
-  deck5: string;
+  Deck1;
+  Deck2;
+  Deck3;
+  Deck4;
+  Deck5;
+  gold: number;
+  defaultDeck: string;
+}
+
+interface Deck1{
+  [deck1: string] : string;
+}
+
+interface Deck2{
+  [deck2: string] : string;
+}
+
+interface Deck3{
+  [deck3: string] : string;
+}
+
+interface Deck4{
+  [deck4: string] : string;
+}
+
+interface Deck5{
+  [deck5: string] : string;
 }
 
 interface Cards{
@@ -49,10 +71,10 @@ export class MainMenuComponent implements OnInit {
         return { id, ...data };
       });
     });
-    this.cards$.subscribe(data => {this.cards=data;console.log(this.cards);console.log(this.cards[0]['effects'][0])});
 
-    if(this.userDoc == null)
-      this.firstLogin();
+    if(this.userDoc == null){
+      this.cards$.subscribe(data => {this.cards=data;this.firstLogin(this.cards);});
+    }
   }
 
   ngOnInit() {
@@ -63,12 +85,49 @@ export class MainMenuComponent implements OnInit {
     this.authService.signOut();
   }
 
-  firstLogin() : void{
+  firstLogin(cards: Object) : void{
+    var cardsIds: Array<string>;
+    cardsIds = [];
+
+    var cardsIdsControl = 0;
+    for(let i in cards){
+      cardsIds[cardsIdsControl] = cards[i]['id'];
+      cardsIds[cardsIdsControl+1] = cards[i]['id'];
+      cardsIdsControl = cardsIdsControl+2;
+    }
+
+    var deck1Cards: Array<string>;
+    deck1Cards = [];
+
+    var random;
+    var deck1Control = cardsIdsControl-1;
+    for(var j = 0; j < 30; j++){
+      random = Math.floor(Math.random() * deck1Control) + 0;
+      if(cardsIds[random] != 'null'){
+        deck1Cards[j] = cardsIds[random];
+        cardsIds[random] = cardsIds[deck1Control];
+        cardsIds[deck1Control] = "null";
+        deck1Control--;
+      }
+    }
+
+    deck1Cards = deck1Cards.sort((a, b) => {
+      if(a < b) return -1;
+      if(a > b) return 1;
+      return 0;
+    });
+
     this.afs.collection('users').doc(this.authService.currentUserId).set({ 
-        'deck1': "vazio",
-        'deck2': "vazio",
-        'deck3': "vazio",
-        'deck4': "vazio",
-        'deck5': "vazio"});
+        'deck1': [deck1Cards[0], deck1Cards[1], deck1Cards[2], deck1Cards[3], deck1Cards[4], deck1Cards[5],
+                  deck1Cards[6], deck1Cards[7], deck1Cards[8], deck1Cards[9], deck1Cards[10], deck1Cards[11],
+                  deck1Cards[12], deck1Cards[13], deck1Cards[14], deck1Cards[15], deck1Cards[16], deck1Cards[17],
+                  deck1Cards[18], deck1Cards[19], deck1Cards[20], deck1Cards[21], deck1Cards[22], deck1Cards[23],
+                  deck1Cards[24], deck1Cards[25], deck1Cards[26], deck1Cards[27], deck1Cards[28], deck1Cards[29]],
+        'deck2': ["vazio"],
+        'deck3': ["vazio"],
+        'deck4': ["vazio"],
+        'deck5': ["vazio"],
+        'gold': 0,
+        'defaultDeck': 'deck1'});
   }
 }
